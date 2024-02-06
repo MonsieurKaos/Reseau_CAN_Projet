b28a5b9d17d0ba9f25d7abcc66df17f8395c16aa
JihaneLaz <157736142+JihaneLaz@users.noreply.github.com>
2024-02-06 09:57:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 #include <iostream>
 #include <string>
 #include "SGP30.hpp"
+#include "CANSmartyCoffre.hpp"
 
+
+#if !DEVICE_CAN
+#error [NOT_SUPPORTED] CAN not supported for this target
+#endif
 
 
 int main()
@@ -23,7 +28,7 @@ int main()
     const char true_msg = 0xFF;
     const char false_msg = 0x00;
     CANMessage m1{0x013,&true_msg,1,CANData, CANStandard};
-    CAN c1{rd,td,32000};
+    CAN c1{rd,td,20000};
     uint8_t r= capteur.test_mesure();
 
     const uint8_t data[2]{0xBE,0xEF};
@@ -36,15 +41,19 @@ int main()
     uint8_t fct10 =capteur.initialiser_mesure(TVOC_mesure_reel, CO2_mesure_reel);
     printf(" retour de initialiser_mesure %d avec mesure TVOC [%d %d] et CO2 [%d %d] ; \n", fct10, TVOC_mesure_reel[0], TVOC_mesure_reel[1], CO2_mesure_reel[0], CO2_mesure_reel[1]);
 
+    CANSmartyCoffre noeud(c1,1);
+
+    noeud.StartPinging();
 
     while (1) {
 
        
         uint8_t fct2= capteur.measure_air_quality(TVOC_mesure, CO2_mesure);
-        uint8_t fct3=c1.write(m1);
-       
+       if ((TVOC_mesure[1]>160) || (TVOC_mesure[0]>1)) {
+            noeud.SendDetectionSignal();
+        }
         printf(" retour de measure_air_quality %d avec mesure TVOC [%d %d] et CO2 [%d %d] ; \n", fct2, TVOC_mesure[0], TVOC_mesure[1], CO2_mesure[0], CO2_mesure[1]);
-        printf(" retour de CAN write %d  ; \n", fct3);
+
 
         ThisThread::sleep_for(1s);
 
