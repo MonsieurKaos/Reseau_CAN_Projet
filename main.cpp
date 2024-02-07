b30dfe98bb61c1ed951d8153a7ee2718266e2d58
mael <mael@morin.fr>
2024-02-07 16:38:33 +0100

    V1.1

--- main.cpp
+++ main.cpp
@@ -5,82 +5,85 @@
 #include "CANSmartyCoffre.hpp"
 #include "stm32l476xx.h"
 
-CAN can1(MBED_CONF_APP_CAN1_RD, MBED_CONF_APP_CAN1_TD);
+// Configuration du contrôleur CAN avec les broches PA_11 et PA_12, vitesse de 20000 bits/s
+CAN can1(PA_11, PA_12, 20000);
 
+// Déclaration de l'interruption liée au bouton poussoir
 InterruptIn Bouton(BUTTON1);
+
+// Timer pour mesurer la durée envoi trame détection CAN
 Timer timerAttente;
+
 float dureeAttente;
 
 bool CmdSuivant;
 
+// Enumération des différents états du programme
 enum Etat {ATTENTE, LECTURE, ENVOI_CAN};
 Etat etatActuel = ATTENTE;
 
+// Fonction appelée lors de l'appui sur le bouton poussoir
 void startPress() {
     CmdSuivant = true;
 }
 
-int map(float x, int in_min, int in_max, int out_min, int out_max) {
-    return static_cast<int>((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min); //static_cast arrondi vers 0
-}
 
 int main() {
 
-    Sharp capteurDroit(PA_0);
-    Sharp capteurGauche(PC_1);
+    // Initialisation du capteurs de distance 
 
+    Sharp capteurDist(PC_1);
+
+    // Association de la fonction startPress() à l'interruption de chute du bouton
     Bouton.fall(&startPress);
 
-    float avgDistanceGauche;
-    int xCoordToSend;
-    float avgDistanceDroite;
-    int yCoordToSend;
+    // Variables pour stocker distance capteur
+    float avgDistance;
 
-    CANSmartyCoffre noeudDistance(can1,1);
+    float DistMinDetect = 15.0;
 
-    noeudDistance.StartPinging();
 
-    while(1){
-         // Intégrer ici la routine keepAlive
+    // Initialisation du nœud CAN pour la communication
+    CANSmartyCoffre noeudDistance(can1, 1);
+    noeudDistance.StartPinging(); // Démarrage de l'envoi de signaux CAN périodiques
+
+    while(1) {
+
         switch(etatActuel) {
-        case ATTENTE:
-        
-            if (CmdSuivant) {
-                CmdSuivant = false;
-                etatActuel = LECTURE;
+            case ATTENTE:
+                if (CmdSuivant) {
+                    CmdSuivant = false;
+                    etatActuel = LECTURE;
+                }
+                break;
+
+            case LECTURE:
+                // Lecture de la distance du capteur 
+                avgDistance = capteurDist;
+                printf("Distance en cm : %d\n", static_cast<int>(avgDistance * 10)); // Affichage de la distance pour le test lecture
                 
-            }
-            break;
+                // Si la distance est inférieure à 15 cm, passer à l'état d'envoi CAN
+                if (avgDistance < DistMinDetect) {
+                    etatActuel = ENVOI_CAN;
+                }
+                break;
 
-
-        case LECTURE:
-
-            avgDistanceGauche = capteurGauche;
-            printf("Distance en cm  %d\n", static_cast<int>(avgDistanceGauche*10)); // pour test doonée et calibrer distance
-            
-        
-            if (avgDistanceGauche<15) {
-                etatActuel = ENVOI_CAN;
-            }
-            break;
-
-        case ENVOI_CAN:
-                
-                printf("Envoi d'une trame de detection CAN \n"); // envoyé ici trame de détection
-                noeudDistance.SendDetectionSignal();
+            case ENVOI_CAN:
+                printf("Envoi d'une trame de détection CAN\n"); // Envoi d'une trame de détection
+                noeudDistance.SendDetectionSignal(); // Envoi d'un signal de détection via CAN
                 timerAttente.start();
+                // Mesure de la durée d'attente
                 dureeAttente = std::chrono::duration<float>(timerAttente.elapsed_time()).count();
-            
+
+                // Si la durée d'attente dépasse 1.0 seconde, retour à l'état d'attente
                 if (dureeAttente >= 1.0) {
-                timerAttente.stop();
-                timerAttente.reset();
-                etatActuel = ATTENTE;
-            }
-            break;
+                    timerAttente.stop();
+                    timerAttente.reset();
+                    etatActuel = ATTENTE;
+                }
+                break;
+        }
 
-
-   }
         ThisThread::sleep_for(10ms);
-    
-}
+    }
 }
